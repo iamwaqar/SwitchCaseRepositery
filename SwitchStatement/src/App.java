19e5f86a30c764b10b50efaa13c55c22f10cfcb6
iamwaqar <iamwaqar1000>
2018-06-21 16:00:03 +0500

    Added new statement

--- SwitchStatement/src/App.java
+++ SwitchStatement/src/App.java
@@ -3,30 +3,45 @@ public class App {
 	public static void main(String[] args) {
 
 		char switchvalue = 'e';
-		switch(switchvalue) {
-		
+		switch (switchvalue) {
+
 		case 'A':
 			System.out.println("Found A");
 			break;
-			
+
 		case 'B':
 			System.out.println("Found B");
 			break;
-			
+
 		case 'C':
 			System.out.println("Found C");
 			break;
-			
+
 		case 'D':
 			System.out.println("Found D");
 			break;
-			
+
 		case 'E':
 			System.out.println("Found E");
 			break;
+
+		default:
+			System.out.println("Nothing found");
+			break;
+		}
+
+		String month = "January";
+		switch (month.toLowerCase()) {
+
+		case "january":
+			System.out.println("January");
+			break;
+		case "febraary":
+			System.out.println("Febraary");
+			break;
 			
 			default:
-				System.out.println("Nothing found");
+				System.out.println("Not found");
 				break;
 		}
 	}
